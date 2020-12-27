e9ab7ebc345c636c9307335d9aac1ff2103683a6
Gaurav Ramrakhyani <gaurav123ram@gmail.com>
2020-12-27 06:36:12 -0800

    added function to reverse string

--- first.go
+++ first.go
@@ -2,8 +2,24 @@ package main
 
 import "fmt"
 
+// function, which takes a string as
+// argument and return the reverse of string.
+func reverse(str string) (result string) {
+	for _, v := range str {
+		result = string(v) + result
+	}
+	return
+}
+
 // Main function
 func main() {
 
+	// Reversing the string.
+	str := "Geeks"
+
+	// returns the reversed string.
+	strRev := reverse(str)
+	fmt.Println(str)
+	fmt.Println(strRev)
 	fmt.Println("!... Hello World ...!")
 }
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/GAURAVRAMRAKHYANI/gotest2
+
+go 1.13
